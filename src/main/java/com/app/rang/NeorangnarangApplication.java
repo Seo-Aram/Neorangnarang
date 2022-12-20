2dead2dc6e361bae2e6d1f7093cd99c16c920637
Seo-Aram <arams.office@gmail.com>
2022-12-20 09:16:49 +0900

    init

---
+++ src/main/java/com/app/rang/NeorangnarangApplication.java
@@ -0,0 +1,13 @@
+package com.app.rang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeorangnarangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeorangnarangApplication.class, args);
+    }
+
+}
