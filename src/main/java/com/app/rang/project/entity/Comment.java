30bb2e59871e8031bfe9d29ea79e32c58f13df75
savesoo <hsw2657@gmail.com>
2022-12-21 17:36:47 +0900

    Update Comment.java
    .

--- src/main/java/com/app/rang/project/entity/Comment.java
+++ src/main/java/com/app/rang/project/entity/Comment.java
@@ -1,6 +1,9 @@
 package com.app.rang.project.entity;
 
+import com.app.rang.project.model.CommentListModel;
 import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.time.LocalDate;
@@ -12,6 +15,8 @@ import java.time.LocalDate;
 @ToString
 @Builder
 @Entity
+@DynamicInsert
+@DynamicUpdate
 @Table(name="boardcomment")
 public class Comment {
 
@@ -20,16 +25,37 @@ public class Comment {
     @Column
     private Long commentidx;
 
+    /*@Column
+    @ManyToOne
+    @JoinColumn(name = "boardidx")
+    private Board boardidx;
+
     @Column
+    @ManyToOne
+    @JoinColumn(name = "useridx")
+    private User useridx;*/
+
+    @Column(columnDefinition = "bigint not null")
     private Long boardidx;
-    @Column
+    @Column(columnDefinition = "bigint not null default 0")
     private Long useridx;
 
     @Column
     private String nickname;
-    @Column
+    @Column(columnDefinition = "text not null")
     private  String content;
-    @Column
+    @Column(columnDefinition = "timestamp not null default current_timestamp()")
     private LocalDate writedate;
+
+    public CommentListModel toCommentView(){
+        return CommentListModel.builder()
+                .commentidx(commentidx)
+                .boardidx(boardidx)
+                .useridx(useridx)
+                .nickname(nickname)
+                .content(content)
+                .writedate(writedate.toString())
+                .build();
+    }
 
 }
